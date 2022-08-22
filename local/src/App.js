6f64ffa1adfe15aa782386c192755ea5d379987c
Priyanka G <prixpriyanka4@gmail.com>
2022-08-22 21:39:18 +0530

    added ICT

--- local/src/App.js
+++ local/src/App.js
@@ -5,12 +5,15 @@ function App() {
     <div>
       <h1>Creating Local Repository</h1>
       <h1>Created Local to Global Repository</h1>
+      <br />
+      <br />
       <h1>
         <u>Cricket Teams</u>
       </h1>
       <h2>England Cricket Team</h2>
       <h2>West Indies Cricket Team</h2>
       <h2>New Zealand National Cricket Team</h2>
+      <h2>Ireland Cricket Team</h2>
     </div>
   );
 }
