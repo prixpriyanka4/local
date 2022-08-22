f43a27cc294f37fbf1f46c153a6baa5a6b9ff500
Priyanka G <prixpriyanka4@gmail.com>
2022-08-22 21:15:10 +0530

    change

---
+++ local/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Creating Local Repository</h1>
+      <h1>Created Local to Global Repository</h1>
+      <h1>Adding Cricketers</h1>
+    </div>
+  );
+}
+
+export default App;
